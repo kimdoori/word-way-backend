6bb9015801064e9fbaa64e207f88c93860215825
kimdoori <dooit0228@gmail.com>
2019-10-13 00:27:34 +0900

    configparser.ConfigParser import 방법 변경

--- word_way/config.py
+++ word_way/config.py
@@ -1,15 +1,15 @@
 """:mod:`word_way.config` --- Word Way application configuration
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
-import configparser
+from configparser import ConfigParser
 from typeguard import typechecked
 
 __all__ = 'load_config',
 
 
 @typechecked
-def load_config(config_name: str) -> configparser.ConfigParser:
-    config = configparser.ConfigParser()
+def load_config(config_name: str) -> ConfigParser:
+    config = ConfigParser()
     # 옵션 이름을 case sensitive 하게 가져오기 위해서
     config.optionxform = str
     config.read(f'conf/{config_name}.conf')
