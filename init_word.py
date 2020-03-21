8d0114f3d6d5efa44c1c0b7d4e19f90df51e60c1
kimdoori <dudu@spoqa.com>
2020-03-22 01:47:11 +0900

    특정 라인부터 읽을 수 있도록 옵션 추가

--- init_word.py
+++ init_word.py
@@ -18,6 +18,10 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('-c', '--config', type=str,
                     default=str(os.environ.get('WORD_WAY_ENV', 'prod')))
+parser.add_argument(
+    '-l', '--line', type=int, default=0,
+    help='Options to determine which line synonyms.tsv should be read from'
+)
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -28,14 +32,16 @@ def main():
     wsgi_app = create_app(args.config)
     with wsgi_app.app_context():
         session = create_session(current_config)
-        scrap_synonyms(session)
+        scrap_synonyms(session, args.line)
 
 
-def scrap_synonyms(session: Session):
+def scrap_synonyms(session: Session, start_line: int):
     """synonyms.tsv 파일을 읽어 유의어 및 단어 정보를 저장하는 함수
 
     :param session: 사용할 세션
     :type session: :class:`sqlalchemy.orm.session.Session`
+    :param start_line: 파일 읽기 시작할 라인
+    :type start_line: :class:`int`
 
     """
     log = logger.getChild('scrap_synonyms')
@@ -43,9 +49,11 @@ def scrap_synonyms(session: Session):
 
     with open(synset_list_dir, newline='') as f:
         rows = csv.reader(f, delimiter='\t')
-        for row in rows:
+        for row_num, row in enumerate(rows):
+            if row_num < start_line:
+                continue
             lemmas_list = [l.strip() for l in row[3].split(',')]
-            log.info(f'Start saving the row ({lemmas_list})')
+            log.info(f'Start saving the row {row_num} ({lemmas_list})')
             pronunciation_ids = []
             for lemmas in lemmas_list:
                 log.info(f'Start saving the words ({lemmas})')
@@ -75,7 +83,7 @@ def scrap_synonyms(session: Session):
                                     ' already exists')
                 log.info(f'Done saving the synonyms ({criteria_id})')
             session.commit()
-            log.info(f'Done saving the row ({lemmas_list})')
+            log.info(f'Done saving the row {row_num} ({lemmas_list})')
 
 
 if __name__ == '__main__':
